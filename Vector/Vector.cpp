33b170e80e167654d0c25c979598a5083118f0cc
Illiadeveloper <dovhal.illia21@gmail.com>
2024-02-28 21:06:48 +0200

    Add _Container_proxy, _Container_base12, _Iterator_base12

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -1,29 +1,48 @@
 ﻿#include <iostream>
+#include <thread>
 #include <vector>
 #include "ymemory.h"
 
-class EmptyClass {};
-
-class _TempFields {
-public:
-    _TempFields() : num(12) {}
-    _STD allocator<int> alloc;
-    int num;
-};
-
-class _TempCompressedPair {
-public:
-    _TempCompressedPair() : pair1(_VEC _Zero_then_variadic_args_t{}, 12) {}
-private:
-    _VEC _Compressed_pair<_STD allocator<int>, int> pair1;
-};
-
 int main() {
-    _TempFields fields;
-    _TempCompressedPair compressedPair;
+    _STD allocator<_VEC _Container_proxy> al1;
+    _STD allocator<_VEC _Container_proxy> al2;
+    _VEC _Container_base12 _Cont1; 
+    _VEC _Container_base12 _Cont2;
+    _Cont1._Alloc_Proxy(al1);
+    _Cont2._Alloc_Proxy(al2);
+    _VEC _Iterator_base12 _iter1; _iter1._Adopt(&_Cont1);
+    _VEC _Iterator_base12 _iter2; _iter2._Adopt(&_Cont2);
+    _VEC _Iterator_base12 _iter3; _iter3._Adopt(&_Cont1);
 
-    std::cout << "Size of _TempFields: " << sizeof(fields) << std::endl;
-    std::cout << "Size of _TempCompressedPair: " << sizeof(compressedPair) << std::endl;
+    std::cout << "_iter1=" << &_iter1 << std::endl;
+    std::cout << "_iter1.Proxy=" << _iter1._MyProxy << std::endl;
+    std::cout << "_iter2=" << &_iter2 << std::endl;
+    std::cout << "_iter2.Proxy=" << _iter2._MyProxy << std::endl;
+    std::cout << "_iter3=" << &_iter3 << std::endl;
+    std::cout << "_iter3.Proxy=" << _iter3._MyProxy << std::endl;
+
+    std::cout << "\n_Cont1.Proxy= " << _Cont1._MyProxy;
+    std::cout << "\n_Cont2.Proxy= " << _Cont2._MyProxy << "\n\n";
+
+    std::cout << "_iter1.nextIter" << _iter1._MyNextIter << std::endl;
+    std::cout << "_iter2.nextIter" << _iter2._MyNextIter << std::endl;
+    std::cout << "_iter3.nextIter" << _iter3._MyNextIter << std::endl;
+
+    _Cont1._Swap_proxy_and_iter(_Cont2);
+
+    std::cout << "_iter1=" << &_iter1 << std::endl;
+    std::cout << "_iter1.Proxy=" << _iter1._MyProxy << std::endl;
+    std::cout << "_iter2=" << &_iter2 << std::endl;
+    std::cout << "_iter2.Proxy=" << _iter2._MyProxy << std::endl;
+    std::cout << "_iter3=" << &_iter3 << std::endl;
+    std::cout << "_iter3.Proxy=" << _iter3._MyProxy << std::endl;
+
+    std::cout << "\n_Cont1.Proxy= " << _Cont1._MyProxy;
+    std::cout << "\n_Cont2.Proxy= " << _Cont2._MyProxy << "\n\n";
+
+    std::cout << "_iter1.nextIter" << _iter1._MyNextIter << std::endl;
+    std::cout << "_iter2.nextIter" << _iter2._MyNextIter << std::endl;
+    std::cout << "_iter3.nextIter" << _iter3._MyNextIter << std::endl;
 
     return 0;
 }
--- Vector/ymemory.h
+++ Vector/ymemory.h
@@ -58,4 +58,195 @@ public:
 		return *_Myval1;
 	};
 };
+
+template<class _Ptrty>
+_NODISCARD constexpr auto _Unfancy(_Ptrty _ptr) {
+	return _STD addressof(*_ptr);
+}
+template<class _Ptr>
+_NODISCARD constexpr _Ptr* _Unfancy(_Ptr* _ptr) {
+	return _ptr;
+}
+
+
+class _Container_base12;
+class _Iterator_base12;
+struct _Container_proxy {
+
+	inline _Container_proxy() noexcept = default;
+	inline _Container_proxy(_Container_base12* _Cont) noexcept 
+		: _MyCont(_Cont) {}
+
+	const _Container_base12* _MyCont = nullptr;
+	mutable _Iterator_base12* _MyfirstIter	= nullptr;
+};
+
+
+class _Container_base12 {
+public:
+	inline _Container_base12() noexcept = default;
+
+	_Container_base12(const _Container_base12&) = delete;
+	_Container_base12& operator=(const _Container_base12&) = delete;
+
+	// destroed all conection with iterator 
+	inline void _Orphan_all() {
+		_Orphan_all_locked();
+	}
+	// Swap proxy
+	inline void _Swap_proxy_and_iter(_Container_base12& _Cont) {
+		if (_Cont._MyProxy == this->_MyProxy) return;
+		_Swap_proxy_and_iter_locked(_Cont);
+	}
+
+	// Create _Container_Proxy12 
+	template<class _Alloc>
+	inline void _Alloc_Proxy(_Alloc&& _Al) {
+		_Container_proxy* _New_Proxy = _Unfancy(_Al.allocate(1));
+		_STD _Construct_in_place(*_New_Proxy, this);
+		_MyProxy			= _New_Proxy;
+		_New_Proxy->_MyCont = this;
+	}
+	// Reload _Container_Proxy12 
+	template<class Alloc>
+	inline void _Reload_Proxy(Alloc&& Old_Al, Alloc&& _New_Al) {
+		_Container_proxy* _New_Proxy = _Unfancy(_New_Al.allocate(1));
+		_STD _Construct_in_place(*_New_Proxy, this);
+		_New_Proxy->_MyCont = this;
+		_STD _Delete_plain_internal(Old_Al, _STD exchange(_MyProxy, _New_Proxy));
+	}
+
+	_Container_proxy* _MyProxy = nullptr;
+private:
+	inline void _Swap_proxy_and_iter_unlocked(_Container_base12&) noexcept;
+	inline void _Orphan_all_unlocked() noexcept;
+
+	inline void _Orphan_all_locked() noexcept {
+		_STD _Lockit lock(_LOCK_DEBUG);
+		_Orphan_all_unlocked();
+	}
+
+	inline void _Swap_proxy_and_iter_locked(_Container_base12& _Cont) noexcept {
+		_STD _Lockit lock(_LOCK_DEBUG);
+		_Swap_proxy_and_iter_unlocked(_Cont);
+	}
+};
+
+class _Iterator_base12 {
+public:
+
+	inline _Iterator_base12() noexcept = default;
+	inline _Iterator_base12(const _Iterator_base12& _Right) noexcept {
+		*this = _Right;
+	}
+
+	inline _Iterator_base12 operator=(const _Iterator_base12& _Right) noexcept {
+		_Assign_locked(_Right);
+	}
+
+	// Create conection to Container
+	inline void _Adopt(const _Container_base12* _Parent) {
+		_Adopt_locked(_Parent);
+	}
+
+	~_Iterator_base12() noexcept
+	{
+		_Orphan_me_locked();
+	}
+
+	inline const _Container_base12* _Cont() const noexcept {
+		return _MyProxy->_MyCont;
+	}
+
+	_Container_proxy* _MyProxy = nullptr;
+	_Iterator_base12* _MyNextIter = nullptr;
+private:
+	void _Adopt_locked(const _Container_base12* _Parent) {
+		_STD _Lockit lock(_LOCK_DEBUG);
+		_Adopt_unlocked(_Parent);
+	}
+
+	void _Assign_locked(const _Iterator_base12& _Right) {
+		_STD _Lockit lock(_LOCK_DEBUG);
+		_Assign_unlocked(_Right);
+	}
+
+	inline void _Orphan_me_locked()
+	{
+		_STD _Lockit lock(_LOCK_DEBUG);
+		_Orphan_me_unlocked();
+	};
+
+	inline void _Orphan_me_unlocked() 
+	{
+		if (!_MyProxy) {
+			return;
+		}
+
+		_Iterator_base12** _Pnext = &_MyProxy->_MyfirstIter;
+		while (!_Pnext && *_Pnext != this) {
+			_Pnext = &(*_Pnext)->_MyNextIter;
+		}
+
+		*_Pnext = _MyNextIter;
+		_MyProxy = nullptr;
+	};
+
+	inline void _Adopt_unlocked(const _Container_base12* _Parent) {
+		if (!_Parent) {
+			_Orphan_me_unlocked();
+			return;
+		}
+
+		_Container_proxy* _Parent_proxy = _Parent->_MyProxy;
+		if (_MyProxy != _Parent_proxy) {
+			if (!_MyProxy) {
+				_Orphan_me_unlocked();
+			}
+
+			_MyNextIter = _Parent_proxy->_MyfirstIter;
+			_Parent_proxy->_MyfirstIter = this;
+			_MyProxy = _Parent_proxy;
+		}
+	}
+
+	inline void _Assign_unlocked(const _Iterator_base12& _Right) {
+		if (_MyProxy == _Right._MyProxy) {
+			return;
+		}
+
+		if (_Right._MyProxy) {
+			_Adopt_unlocked(_Right._MyProxy->_MyCont);
+		}
+		else {
+			_Orphan_me_unlocked();
+		}
+	}
+};
+
+
+inline void _Container_base12::_Swap_proxy_and_iter_unlocked(_Container_base12& _Cont) noexcept {
+	_Container_proxy* temp = _MyProxy;
+	_MyProxy = _Cont._MyProxy;
+	_Cont._MyProxy = temp;
+
+	if (_MyProxy) {
+		_MyProxy->_MyCont = this;
+	}
+
+	if (_Cont._MyProxy) {
+		_Cont._MyProxy->_MyCont = &_Cont;
+	}
+}
+
+inline void _Container_base12::_Orphan_all_unlocked() noexcept {
+	if (!_MyProxy) {
+		return;
+	}
+
+	for (auto _Pnext = _STD exchange(_MyProxy->_MyfirstIter, nullptr); _Pnext; _Pnext = _Pnext->_MyNextIter) {
+		_Pnext->_MyProxy = nullptr;
+	}
+}
+
 _VEC_END
